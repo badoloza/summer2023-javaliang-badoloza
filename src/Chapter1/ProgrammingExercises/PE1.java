bb10bb3266a7d26ea144cd193b5709a72b4f8e21
baroloza <eduardoloza95@gmail.com>
2023-06-01 23:50:16 -0700

    completed programming exercise 1.1

---
+++ src/Chapter1/ProgrammingExercises/PE1.java
@@ -0,0 +1,23 @@
+package Chapter1.ProgrammingExercises;
+
+/**
+ * Requirements Specification:
+ * Program must display:
+ * "Welcome to Java",
+ * "Welcome to Computer Science",
+ * "Programming is fun"
+ * <p>
+ * System Analysis:
+ * The program outputs 3 specific messages to console
+ * <p>
+ * System Design:
+ * Use System class to print each message
+ */
+
+public class PE1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
