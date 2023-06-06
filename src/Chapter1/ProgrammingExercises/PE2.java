b4b04986d8977836e8b51d4e22c8bc2f13bcb64b
baroloza <eduardoloza95@gmail.com>
2023-06-05 22:49:16 -0700

    completed and implemented programming exercise 2

---
+++ src/Chapter1/ProgrammingExercises/PE2.java
@@ -0,0 +1,22 @@
+/**
+ * Requirements Specification:
+ * Program must display "Welcome to Java" 5 times.
+ *
+ * System Analysis:
+ * Program outputs "Welcome to Java" 5 times to console
+ *
+ * System Design:
+ * Use System class to print "Welcome to Java" 5 times
+ */
+
+package Chapter1.ProgrammingExercises;
+
+public class PE2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
