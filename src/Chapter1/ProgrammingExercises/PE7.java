28320274900f7f05384a42288cfd370ead6c08ca
baroloza <eduardoloza95@gmail.com>
2023-06-25 21:45:16 -0700

    completed programming exercise 7 and 8

---
+++ src/Chapter1/ProgrammingExercises/PE7.java
@@ -0,0 +1,15 @@
+/*
+ * This program displays the result of an approximation of PI
+ * Using these formulas:
+ * 4 * (1 - (1 / 3) + (1 / 5) - (1 / 7) + (1 / 9) - (1 / 11))
+ * 4 * (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13))
+ */
+
+package Chapter1.ProgrammingExercises;
+
+public class PE7 {
+    public static void main(String[] args) {
+        System.out.println(4 * (1 - (1 / 3) + (1 / 5) - (1 / 7) + (1 / 9) - (1 / 11)));
+        System.out.println(4 * (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13)));
+    }
+}
---
+++ src/Chapter1/ProgrammingExercises/PE8.java
@@ -0,0 +1,18 @@
+/*
+ * This program displays the area and perimeter of circle
+ * with radius 5.5
+ * Using the following formula:
+ * perimeter = 2 * radius * pi
+ * area = radius * radius * pi
+ */
+
+package Chapter1.ProgrammingExercises;
+
+public class PE8 {
+    public static void main(String[] args) {
+        System.out.print("perimeter = ");
+        System.out.println(2 * 5.5 * 3.14);
+        System.out.print("area = ");
+        System.out.println(5.5 * 5.5 * 3.14);
+    }
+}
