06df2640d57f4f7409c99892c6bc316b9811e16d
baroloza <eduardoloza95@gmail.com>
2023-06-27 00:50:54 -0700

    added programming exercise 12 and introduced javadoc comments

---
+++ src/Chapter_01/Exercise01_12.java
@@ -0,0 +1,18 @@
+/**
+ * @description
+ * This program displays the average speed of a runner
+ * that runs 24 miles in 1 hour, 45 minutes, and 35 seconds
+ * in kilometers
+ * 1 mile is 1.6 kilometers
+ *
+ * @author badoloza
+ * @version 1.0
+ * @since 06-27-2023
+ */
+
+package Chapter_01;
+
+public class Exercise01_12 {
+    public static void main(String[] args) {
+    }
+}
